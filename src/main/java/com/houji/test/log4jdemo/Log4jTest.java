ef0efe1ebb63b4c4efca811b88fa1c88ec0137c0
houmingcheng <houmingchenguu@163.com>
2019-06-27 14:48:14 +0800

    1

---
+++ src/main/java/com/houji/test/log4jdemo/Log4jTest.java
@@ -0,0 +1,17 @@
+package com.houji.test.log4jdemo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Log4jTest {
+    private static Logger logger = LoggerFactory.getLogger(Log4jTest.class);
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            int age=0;
+            logger.debug("输出i:{},age:{}", i,age);
+        }
+
+
+    }
+}
